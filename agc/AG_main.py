510ba0aa076afc678a4ce29f90281864fd2349e3
Eric <eric.lopezs@alumnos.udg.mx>
2020-11-22 20:20:59 -0600

    Add Rastrigin problem

--- agc/AG_main.py
+++ agc/AG_main.py
@@ -1,13 +1,15 @@
 import sphere
+import rastrigin
 import GeneticAlgorithm
 
 def main():
     sphere_problem = sphere.Sphere()
+    rastrigin_problem = rastrigin.Rastrigin()
     allels = 2
     population_size = 32
     generations = 200000
     mutation_rate = 0.1
-    ag = GeneticAlgorithm.GeneticAlgorithm(population_size, allels, generations, mutation_rate, sphere_problem)
+    ag = GeneticAlgorithm.GeneticAlgorithm(population_size, allels, generations, mutation_rate, rastrigin_problem)
     ag.run()
 
 if __name__ == '__main__':
